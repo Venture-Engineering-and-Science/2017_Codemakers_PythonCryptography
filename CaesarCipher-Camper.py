229abe69a79e9cd2c399df2e7d4cc382f631bd1f
Andrew Berriault <berriaak@mcmaster.ca>
2017-07-16 12:34:37 -0400

    Create CaesarCipher-Camper.py

---
+++ CaesarCipher-Camper.py
@@ -0,0 +1,33 @@
+# Filename: CaesarCipher
+# Camp: Venture Codemakers Advanced
+# Date: Summer 2017
+# Camper Name: 
+
+
+#### Problem #1: Encoding messages so that they are more secure. ####
+#
+# Create a Function named encode that takes two arguments: text and key.
+# 
+# Python functions use the syntax
+# def functionName (arguments):
+#     The function code here
+#
+#     return variable
+
+
+#### Problem #2: Decoding messages. ####
+#
+# Create a Function named decode that takes two arguments: text and key.
+# This function should take the text and decode it using the key value provided.
+
+def decode (text, key):
+    # Your Code Here
+
+
+#### Problem #3: Decoding messages without the key. ####
+#
+# Create a Function that decodes the message using brute force. The best way to
+# do this is to try all 26 combinations and let the user figure out what the likely message is.
+
+def brute (text):
+    # Your Code Here
